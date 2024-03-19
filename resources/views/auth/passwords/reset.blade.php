9cc13852ee514235deff349d7056801578a45e3f
Guilherme <guilherme.ramos@lsmais.com.br>
2024-03-18 22:21:34 -0300

    ajustes

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,90 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<div class="container-fluid d-flex align-items-center justify-content-center">
+    <div class="col-lg-3  rounded text-center p-3" style="background-color: rgba(255, 255, 255, 0.6);">
+        <span class="h2">SAT-TCC</span>
+        <form method="POST" action="{{ route('password.update') }}">
+            @csrf
+            <input type="hidden" name="token" value="{{ $token }}">
+            <div class="">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email" autofocus>
+                
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+                
+                <div class="form-group">
+                    <div class="password-icon password">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror mt-2" name="password" required autocomplete="current-password" placeholder="Senha">
+                        <i onclick="showPassword(event)" id="eyeIcon" class="fa fa-eye mt-2"></i>
+                    </div>
                 </div>
+                @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+                <div class="form-group">
+                    <div class="password-icon password">
+                        <input id="password_confirmation" type="password" class="form-control @error('password_confirmation') is-invalid @enderror mt-2" name="password_confirmation" required autocomplete="current-password" placeholder="Confirmar Senha">
+                        <i onclick="showPassword(event)" id="eyeIcon" class="fa fa-eye mt-2"></i>
+                    </div>
+                </div>
+                @error('password_confirmation')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             </div>
-        </div>
+            <button type="submit" class="btn btn-gold w-50 my-2">
+                {{ __('Entrar') }}
+            </button>
+        </form>
     </div>
 </div>
 @endsection
+@push('scripts')
+    <script>
+        function showPassword(event) {
+            const element = event.target.parentNode.querySelector('input');
+            const eyeIcon = document.getElementById('eyeIcon');
+
+            element.type = element.type === 'password' ? 'text' : 'password';
+            eyeIcon.classList.toggle('fa-eye-slash', element.type === 'text');
+            eyeIcon.classList.toggle('fa-eye', element.type === 'password');
+        }
+    </script>
+@endpush
+@push('css')
+<style>
+    .container-fluid {
+        background-color:#243147;
+        background-repeat: no-repeat;
+        background-size: cover;
+        width: 100vw;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        left: 0;
+    }
+    .password-icon {
+            position: relative;
+            display: inline-block;
+            width: 100%;
+        }
+
+        .password-icon i {
+            position: absolute;
+            top: 20px;
+            right: 10px;
+            transform: translateY(-50%);
+            cursor: pointer;
+        }
+    .rounded {
+        border-radius: 10px !important;
+    }
+</style>
+@endpush
